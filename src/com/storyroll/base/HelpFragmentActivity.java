2f33374c7d1e3d4c9a3a736f939dfe8570ba3438
Martynas Savickis <martynas@ore.lt>
2014-04-01 10:59:03 +0800

    first version of help #79

---
+++ src/com/storyroll/base/HelpFragmentActivity.java
@@ -0,0 +1,12 @@
+package com.storyroll.base;
+
+import android.widget.TextView;
+
+import com.storyroll.R;
+
+public class HelpFragmentActivity extends MenuFragmentActivity {
+	protected void initHelp(){
+		TextView ht = (TextView)findViewById(R.id.helpText);
+		
+	}
+}
